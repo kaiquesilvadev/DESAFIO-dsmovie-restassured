08ccb40ab73c58cfbb3235a3f2ab242d69950028
kaiquesilvadev <j.kaique1570@gmail.com>
2024-03-03 19:50:39 -0300

    teste de api , insertShouldReturnUnprocessableEntityWhenAdminLoggedAndBlankTitle ok

--- src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
+++ src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -3,9 +3,12 @@ package com.devsuperior.dsmovie.controllers;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.CoreMatchers.is;
-import static org.mockito.Mockito.when;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.json.JSONException;
+import org.json.simple.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,6 +20,8 @@ import io.restassured.RestAssured;
 
 public class MovieControllerRA {
 	
+	private Map<String, Object> postMovieInstance;
+	
 	private String clientUsername, clientPassword, adminUsername, adminPassword;
 	private String adminToken, clientToken, invalidToken;
 	
@@ -25,6 +30,13 @@ public class MovieControllerRA {
 
 	@BeforeEach
 	void setUp() throws Exception {
+		
+		postMovieInstance = new HashMap<>();
+		postMovieInstance.put("title", "eu sou a lenda");
+		postMovieInstance.put("score", 2.3);
+		postMovieInstance.put("count", 3);
+		postMovieInstance.put("imgUrl", "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg");
+		
 		
 		clientUsername = "alex@gmail.com";
 		clientPassword = "123456";
@@ -89,8 +101,20 @@ public class MovieControllerRA {
 			.statusCode(HttpStatus.NOT_FOUND.value());
 	}
 	
+	@DisplayName("insert deve retornar entidade não processável quando o admin estiver logado e título em branco")
 	@Test
 	public void insertShouldReturnUnprocessableEntityWhenAdminLoggedAndBlankTitle() throws JSONException {		
+		postMovieInstance.put("title", "");
+		JSONObject newProduct = new JSONObject(postMovieInstance);
+		
+		RestAssured.given()
+		.header("Content-type", "application/json")
+		.header("Authorization", "Bearer " + adminToken)
+		.body(newProduct)
+		.when()
+			.post("/movies")
+		.then()
+			.statusCode(HttpStatus.UNPROCESSABLE_ENTITY.value());
 	}
 	
 	@Test
